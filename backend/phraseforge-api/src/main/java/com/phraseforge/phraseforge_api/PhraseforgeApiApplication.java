a0882d6cb101dcca9cdbd0412947182d23ac842c
joaovv-vitor <jrodriguescontatato@gmail.com>
2026-08-17 13:12:07 -0300

    chore(init): initialize project structure

---
+++ backend/phraseforge-api/src/main/java/com/phraseforge/phraseforge_api/PhraseforgeApiApplication.java
@@ -0,0 +1,13 @@
+package com.phraseforge.phraseforge_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhraseforgeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhraseforgeApiApplication.class, args);
+	}
+
+}
---
+++ backend/phraseforge-api/src/test/java/com/phraseforge/phraseforge_api/PhraseforgeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phraseforge.phraseforge_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhraseforgeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
